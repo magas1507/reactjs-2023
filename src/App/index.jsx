e80ad9993fd2e2e360a9babc41640fba03a903f6
magas1507 <asmg15.07@gmail.com>
2023-08-03 21:38:48 -0300

    useContext vs consumer(old)

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
-function App() {
-  
-  
+function App() { 
   return (
     <TodoProvider>
       <AppUI/>
